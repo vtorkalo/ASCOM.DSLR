781513ddaf61009d4b4ba6bcf4c44005fe98905d
SOFTSERVE\vtork <vtork@softserveinc.com>
2018-07-23 11:28:46 +0300

    Bat file for installer build

---
+++ build.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "./ASCOM.DSLR.sln" /p:configuration=debug /property:Platform="x86"
+
+"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" "./DSLR.Camera Setup.iss"
+
+pause
