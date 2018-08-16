0fe12d15229b10c763a83322bf3bb7fd8c115fc2
rwg0 <robin.glover@workshare.com>
2018-08-16 18:50:40 +0100

    Add registrar project - not working yet

---
+++ Registrar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe", "/codebase ASCOM.DSLR.dll");
+            Process.Start(@"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe", "/codebase ASCOM.DSLR.dll");
+        }
+    }
+}
